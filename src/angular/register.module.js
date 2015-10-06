9493ac34bd6ba962e5d8399338227612b7f364a8
Luke Kysow <lkysow@gmail.com>
2015-10-05 22:54:25 -0700

    Revert "Revert changes made for review system. Changes will be made on another branch"
    This reverts commit f2833d8d97923125cde6673a0fc05728c0e47996.
    This updates us with master, and then reverts the previous revert so we can apply commits on
    top safely

--- src/angular/register.module.js
+++ src/angular/register.module.js
@@ -2,7 +2,7 @@
 // with their dependencies
 
 /*** Routes ***/
-angular.module('servicesAdvisorApp', ['ngRoute', 'controllers', 'services', 'pascalprecht.translate']);
+angular.module('servicesAdvisorApp', ['ngRoute', 'controllers', 'services', 'directives', 'pascalprecht.translate']);
 
 /*** Services ***/
 angular.module('services', ['ngResource','underscore']);
@@ -10,12 +10,14 @@ angular.module('services', ['ngResource','underscore']);
 /*** controllers ***/
 angular.module('controllers', ['underscore']);
 
+/*** directives ***/
+angular.module('directives', []);
 
-/*** registering  underscore.js helper ***/ 
 
-// Just inject into where you need it like a service 
+/*** registering  underscore.js helper ***/
+
+// Just inject into where you need it like a service
 
 // documentation: http://underscorejs.org/
 
 angular.module('underscore', []);
-
