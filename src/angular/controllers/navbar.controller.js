29f81b505b7ac2bbf7206b136f56f3a12605ba51
Luke Kysow <lkysow@gmail.com>
2015-12-20 17:04:30 -0800

    Simplify toggling of filter overlay and closing when clicking outside

--- src/angular/controllers/navbar.controller.js
+++ src/angular/controllers/navbar.controller.js
@@ -3,7 +3,7 @@ var controllers = angular.module('controllers');
 controllers.controller('NavbarCtrl', ['$scope', 'Cookies', function ($scope, Cookies) {
   $scope.languages = ['AR','EN'];
   $scope.selectedLanguage = Cookies.getCookie('LANGUAGE') || 'AR';
-  
+
   $scope.changeLanguage = function (langKey) {
     // we just set the cookie and reload since things aren't set up to properly reload new services list
     Cookies.setCookie('LANGUAGE', langKey);
@@ -13,94 +13,16 @@ controllers.controller('NavbarCtrl', ['$scope', 'Cookies', function ($scope, Coo
     window.location = "/";
   };
 
-  $(".filter-pill").click(function(event) {
-    //TODO
-    event.stopPropagation();
-  });
-
   $scope.toggleFilters = toggleFilters;
 
+  // when anywhere outside the filters overlay is clicked, close the filters
+  $('body').on("click", function(e) {
+    if (e.target.id !== "filtersButton" && e.target.id !== "filters" && $(e.target).parents("#filters").size() == 0) {
+      $("#filters").removeClass('active')
+    }
+  })
 }]);
 
-toggleFilters = (function(){
-
-    // Local variables defined 
-    var $filter = $('#filters');
-    var $overlay = $('.overlay-tint');
-    var $body = $('body');     
-    var $mapContainer  = $('#mapContainer');
-    var $serviceList  = $('#serviceList');
-    // check for 'active' class from the #filter DOM element
-    var activeClass = $('#filters').attr('class').split(/ /)[1];  
-    
-    var _reloadVariables = function(){
-        $filter = $('#filters');                
-        activeClass = $('#filters').attr('class').split(/ /)[1];       
-    }
-
-    var _bindCloseListners = function(){
-        $mapContainer.on('click',function(){
-            $filter.removeClass('active');
-            $overlay.removeClass('active');
-        })
-        $serviceList.on('click',function(){
-            $filter.removeClass('active');
-            $overlay.removeClass('active');
-        })
-
-        $overlay.on('click', function(){
-            $filter.removeClass('active');
-            $overlay.removeClass('active');  
-        })
-    }
-
-    _bindCloseListners();
-
-    var _toggleFilter = function(){ 
-        $body.on('click',function(event){    
-            var nonActiveFilterBtn =  activeClass !== 'active' && event.target.id == 'filtersButton';
-            var activeFilterBtn =  activeClass == 'active' && event.target.id == 'filtersButton';
-            var activeApplyFilterBtn = activeClass == 'active' && event.target.id == 'applyFilter';            
-
-            if (nonActiveFilterBtn){ 
-                $filter.addClass('active'); 
-                $overlay.addClass('active');
-            }
-            else if(activeFilterBtn || activeApplyFilterBtn){ 
-                $filter.removeClass('active'); 
-                  $overlay.removeClass('active');
-            }                   
-        })
-    }
-
-    var _exec = function(){    
-        _reloadVariables();      
-        // wait for DOM variable reload
-        setTimeout(_toggleFilter(), 250);
-    }
-
-    return {
-        exec : _exec
-    }
-
-})()
-
-// Global so that filters.controller can access this via its scope
-// TODO: Should probably put this somewhere else
-// toggleFilters = function() {
-//     var $filter = $('#filters');
-
-//     $('body').on('click',function(e){
-//         var targetElementId = e.target.id;
-//         var activeClass = $filter.attr('class').split(/ /)[1];
-                        
-//         if (targetElementId == 'filtersButton' && activeClass !== 'active'){
-//             $filter.addClass('active');            
-//             console.log('active again.....');
-//         }else {             
-//             $('#filters').removeClass('active').removeClass('active');             
-//             console.log(activeClass);
-//         }
-
-//     })
-// };
+toggleFilters = function () {
+  $('#filters').toggleClass('active');
+};
