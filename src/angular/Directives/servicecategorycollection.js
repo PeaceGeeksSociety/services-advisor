3c135c5de875f2f828343e9cf5463c24e6816168
Smith Milner <smithmilner@gmail.com>
2017-05-04 13:55:09 -0700

    See #48. Limiting services provided to root categories and leaf categories.

--- src/angular/Directives/servicecategorycollection.js
+++ src/angular/Directives/servicecategorycollection.js
@@ -7,7 +7,45 @@ directives.directive('serviceCategoryCollection', [function() {
         scope: {
             collection: '='
         },
-        templateUrl: 'src/angular/Views/components/service-category-collection.html'
+        templateUrl: 'src/angular/Views/components/service-category-collection.html',
+        link: function (scope, element, attr) {
+            var newCollection = scope.collection.slice();
+            if (angular.isUndefined(attr.reduce)) {
+                attr.reduce = true;
+            }
+
+            function reduce(collection) {
+                // if depth is 0 or there are no children display it.
+                // if there are children flatten them.
+                var newCollection = [];
+
+                angular.forEach(collection, function(value, key) {
+                    var item = {};
+                    item.name = value.name;
+                    item.depth = value.depth;
+                    item.children = [];
+
+                    if (value.depth === 0 || value.children.length === 0) {
+                        newCollection.push(item);
+                    }
+
+                    if (value.depth === 0 && value.children.length > 0) {
+                        item.children = reduce(value.children);
+                    }
+
+                    if (value.depth !== 0 && value.children.length > 0) {
+                        newCollection = newCollection.concat(reduce(value.children));
+                    }
+
+                });
+
+                return newCollection;
+            }
+
+            if (attr.reduce) {
+                scope.collection = reduce(newCollection);
+            }
+        }
     };
 }])
 .directive('serviceCategoryMember', ['$compile', function($compile) {
@@ -20,7 +58,7 @@ directives.directive('serviceCategoryCollection', [function() {
         template: '<li>{{ member.name }}</li>',
         link: function (scope, element, attrs) {
             if (angular.isArray(scope.member.children)) {
-                element.append("<service-category-collection collection='member.children'></service-category-collection>");
+                element.append("<service-category-collection reduce='false' collection='member.children'></service-category-collection>");
                 $compile(element.contents())(scope);
             }
         }
