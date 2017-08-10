57605f7075dcb36325edce08ff47479c3c1fceae
Smith Milner <smithmilner@gmail.com>
2017-08-10 17:44:03 -0400

    Adding details about current search to results page.

--- src/angular/controllers/results.controller.js
+++ src/angular/controllers/results.controller.js
@@ -3,7 +3,7 @@ var controllers = angular.module('controllers');
 /**
  * For the results view
  */
-controllers.controller('ResultsCtrl', ['$scope', '$location', '$translate', '_', 'Search', 'ServicesList', function ($scope, $location, $translate, _, Search, ServicesList) {
+controllers.controller('ResultsCtrl', ['$scope', '$location', '$translate', '_', 'Search', 'ServicesList', 'SectorList', function ($scope, $location, $translate, _, Search, ServicesList, SectorList) {
 
     // A bit of a hack to get the services to load before we apply any filter on,
     // ServicesList.get will only load the services if they haven't been loaded already.
@@ -11,6 +11,13 @@ controllers.controller('ResultsCtrl', ['$scope', '$location', '$translate', '_',
         function(services){
             // ****** RESULTS OBJECT *********
             $scope.results = Search.filterByUrlParameters();
+
+            var search = $location.search();
+            var sectorIds = search.category || [];
+
+            SectorList.findAll(sectorIds, function (sectors) {
+                $scope.categories = sectors;
+            });
         }
     );
 
